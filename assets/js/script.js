17c817be1f20901426f7660ce3c32c03ff49ab00
Evan Reissmann <evanreissmann4@yahoo.com>
2021-12-14 17:54:21 -0500

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 function movieTitleSearch() {
-const title=document.getElementById("title");
+const title=document.getElementById("title").value;
+console.log(`https://imdb-api.com/API/SearchTitle/k_u5eiz1tf/${title}`);
 console.log("here");
-fetch(`https://imdb-api.com/API/SearchTitle/k_u5eiz1tf/${title}`, { method: "get", headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } }).then(function(response){
-    console.log(response);
+fetch(`https://imdb-api.com/API/SearchTitle/k_u5eiz1tf/${title}`, { method: "get", headers: {"Content-Type": "application/json"}}).then(async function(response){
+    const jsonresponse=await response.json();
+console.log(jsonresponse);
 }).catch(function(err) {
     console.log(err);
 });
