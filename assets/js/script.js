e279e5520b4c6a5b40fcdcdbf187c6a75aa00774
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2021-12-14 20:46:07 -0500

    setting up api call for yelp

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,22 @@ console.log(jsonresponse);
     console.log(err);
 });
 
+// var axios = require("axios").default;
+
+var options = {
+  method: 'POST',
+  url: ('https://yelp-graphql1.p.rapidapi.com/v3/graphql'),
+  headers: {
+    authorization: 'Bearer  wv8UFPtBvt4bjBpvlRAaloi5S1owaCvTY_k9t2yfWJujEKIzuexgv9Q8krD70-wofjvNFQ9hsM4Js3w1e4Go_ZzF7TAelCoun3in5WFIUXCjQeVF79-RUuGegTu5YXYx',
+    'x-rapidapi-host': 'yelp-graphql1.p.rapidapi.com',
+    'x-rapidapi-key': '07c51671f2mshff850560144c582p1006f5jsn1dbae7cf897d'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 } 
